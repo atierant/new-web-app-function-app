cb839717400c4f87ef90a673fc592fc4a375bb50
Arrnaud TIERANT <arnaud@tierant.fr>
2024-06-30 17:01:13 +0200

    add_cred

--- HttpTriggerFunctionApp.cs
+++ HttpTriggerFunctionApp.cs
@@ -1,6 +1,18 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using System;
+
 public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
 {
     log.LogInformation("C# HTTP trigger function processed a request.");
+
+    var username =  Environment.GetEnvironmentVariable("UsernameFromKeyVault", EnvironmentVariableTarget.Process);
+    var password =  Environment.GetEnvironmentVariable("PasswordFromKeyVault", EnvironmentVariableTarget.Process);
+
+    log.LogInformation($"Username: {username}");
+    log.LogInformation($"Username: {password}");
 
     string name = req.Query["name"];
 
